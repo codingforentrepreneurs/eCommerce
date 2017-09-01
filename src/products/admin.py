311291431e55a783532ff3cd9319e22d77ff92c0
CFE <hello@teamcfe.com>
2017-09-01 12:16:51 -0700

    4 - Django Admin

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+admin.site.register(Product)
--- src/products/models.py
+++ src/products/models.py
@@ -5,3 +5,9 @@ class Product(models.Model):
     title           = models.CharField(max_length=120)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
+
+    def __str__(self):
+        return self.title
+
+    def __unicode__(self):
+        return self.title
