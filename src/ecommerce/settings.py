a80ded7e6ed9a87da9a8029e4e6fe50c18fd4b79
CFE <hello@teamcfe.com>
2017-09-01 12:08:44 -0700

    3 - Product Model

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #our apps
+    'products',
 ]
 
 MIDDLEWARE = [
--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title           = models.CharField(max_length=120)
+    description     = models.TextField()
+    price           = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
