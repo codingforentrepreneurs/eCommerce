f146e4775cb274176897f8e06d842150599aa3c5
CFE <hello@teamcfe.com>
2017-09-01 15:38:24 -0700

    9 - Custom Model Managers

--- src/products/models.py
+++ src/products/models.py
@@ -19,11 +19,20 @@ def upload_image_path(instance, filename):
             final_filename=final_filename
             )
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id=id) # Product.objects == self.get_queryset()
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 class Product(models.Model):
     title           = models.CharField(max_length=120)
     description     = models.TextField()
     price           = models.DecimalField(decimal_places=2, max_digits=20, default=39.99)
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
+
+    objects = ProductManager()
 
     def __str__(self):
         return self.title
--- src/products/views.py
+++ src/products/views.py
@@ -8,13 +8,16 @@ from .models import Product
 
 
 class ProductListView(ListView):
-    queryset = Product.objects.all()
     template_name = "products/list.html"
 
     # def get_context_data(self, *args, **kwargs):
     #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
     #     print(context)
     #     return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
 
 
 def product_list_view(request):
@@ -26,7 +29,7 @@ def product_list_view(request):
 
 
 class ProductDetailView(DetailView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
 
     def get_context_data(self, *args, **kwargs):
@@ -34,6 +37,19 @@ class ProductDetailView(DetailView):
         print(context)
         # context['abc'] = 123
         return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product doesn't exist")
+        return instance
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     return Product.objects.filter(pk=pk)
 
 
 def product_detail_view(request, pk=None, *args, **kwargs):
@@ -47,12 +63,17 @@ def product_detail_view(request, pk=None, *args, **kwargs):
     # except:
     #     print("huh?")
 
-    qs  = Product.objects.filter(id=pk)
-    #print(qs)
-    if qs.exists() and qs.count() == 1: # len(qs)
-        instance = qs.first()
-    else:
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
         raise Http404("Product doesn't exist")
+    #print(instance)
+    # qs  = Product.objects.filter(id=pk)
+
+    # #print(qs)
+    # if qs.exists() and qs.count() == 1: # len(qs)
+    #     instance = qs.first()
+    # else:
+    #     raise Http404("Product doesn't exist")
 
     context = {
         'object': instance
