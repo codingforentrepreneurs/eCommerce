f68b6c15804033937d823f6a707df363901fd6a1
CFE <hello@teamcfe.com>
2017-09-18 11:48:31 -0700

    3 Pass Arguments with Include

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -26,7 +26,7 @@ def contact_page(request):
     context = {
         "title":"Contact",
         "content":" Welcome to the contact page.",
-        "form": contact_form
+        "form": contact_form,
     }
     if contact_form.is_valid():
         print(contact_form.cleaned_data)
