d0e38edee652c5787dc7bf12e2ec2909fa3121f8
CFE <hello@teamcfe.com>
2017-09-18 15:01:43 -0700

    3 - Rows And Columns

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls.static import static
 
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 # from products.views import (
 #         ProductListView, 
@@ -38,6 +39,7 @@ urlpatterns = [
     url(r'^contact/$', contact_page, name='contact'),
     url(r'^login/$', login_page, name='login'),
     url(r'^register/$', register_page, name='register'),
+    url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^products/', include("products.urls", namespace='products')),
     # url(r'^featured/$', ProductFeaturedListView.as_view()),
     # url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
