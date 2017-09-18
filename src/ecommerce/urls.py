fc97ee8fd094a47aeb47ad7b25cf2d5a04c78fe8
CFE <hello@teamcfe.com>
2017-09-18 12:18:47 -0700

    6 - Navbar

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -33,11 +33,11 @@ from django.contrib import admin
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
-    url(r'^$', home_page),
-    url(r'^about/$', about_page),
-    url(r'^contact/$', contact_page),
-    url(r'^login/$', login_page),
-    url(r'^register/$', register_page),
+    url(r'^$', home_page, name='home'),
+    url(r'^about/$', about_page, name='about'),
+    url(r'^contact/$', contact_page, name='contact'),
+    url(r'^login/$', login_page, name='login'),
+    url(r'^register/$', register_page, name='register'),
     url(r'^products/', include("products.urls", namespace='products')),
     # url(r'^featured/$', ProductFeaturedListView.as_view()),
     # url(r'^featured/(?P<pk>\d+)/$', ProductFeaturedDetailView.as_view()),
