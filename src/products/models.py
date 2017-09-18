dd4978699bbc5f82870616d4f22bd7c0bc19c055
CFE <hello@teamcfe.com>
2017-09-18 12:31:04 -0700

    |linebreaks

--- src/products/models.py
+++ src/products/models.py
@@ -56,6 +56,7 @@ class Product(models.Model):
     image           = models.ImageField(upload_to=upload_image_path, null=True, blank=True)
     featured        = models.BooleanField(default=False)
     active          = models.BooleanField(default=True)
+    timestamp       = models.DateTimeField(auto_now_add=True)
 
     objects = ProductManager()
 
