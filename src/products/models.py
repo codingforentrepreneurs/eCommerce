54e65447fd0cd9b269c76ac1c9739af9cb1cd7ce
CFE <hello@teamcfe.com>
2017-09-20 16:33:06 -0700

    4 - Better Lookups with Q

--- src/products/models.py
+++ src/products/models.py
@@ -1,6 +1,7 @@
 import random
 import os
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 from django.urls import reverse
 
@@ -30,6 +31,12 @@ class ProductQuerySet(models.query.QuerySet):
     def featured(self):
         return self.filter(featured=True, active=True)
 
+    def search(self, query):
+        lookups = (Q(title__icontains=query) | 
+                  Q(description__icontains=query) |
+                  Q(price__icontains=query))
+        return self.filter(lookups).distinct()
+
 class ProductManager(models.Manager):
     def get_queryset(self):
         return ProductQuerySet(self.model, using=self._db)
@@ -46,6 +53,8 @@ class ProductManager(models.Manager):
             return qs.first()
         return None
 
+    def search(self, query):
+        return self.get_queryset().active().search(query)
 
 
 class Product(models.Model):
--- src/search/views.py
+++ src/search/views.py
@@ -17,7 +17,7 @@ class SearchProductView(ListView):
         method_dict = request.GET
         query = method_dict.get('q', None) # method_dict['q']
         if query is not None:
-            return Product.objects.filter(title__icontains=query) 
+            return Product.objects.search(query)
         return Product.objects.featured()
         '''
         __icontains = field contains this
