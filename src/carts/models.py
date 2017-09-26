3f9592bd3209ae09d5aa839c87356d7c1c6bc1bd
CFE <hello@teamcfe.com>
2017-09-25 17:58:49 -0700

    5 - Cart Model Manager

--- src/carts/models.py
+++ src/carts/models.py
@@ -7,6 +7,13 @@ from products.models import Product
 
 User = settings.AUTH_USER_MODEL
 
+class CartManager(models.Manager):
+    def new(self, user=None):
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated():
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
 
 class Cart(models.Model):
     user        = models.ForeignKey(User, null=True, blank=True)
@@ -14,6 +21,8 @@ class Cart(models.Model):
     total       = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
 
     def __str__(self):
         return str(self.id)
--- src/carts/views.py
+++ src/carts/views.py
@@ -2,24 +2,16 @@ from django.shortcuts import render
 
 from .models import Cart
 
-
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    print('New Cart created')
-    return cart_obj
-
 def cart_home(request):
-    request.session['cart_id']  = "12"
     cart_id = request.session.get("cart_id", None)
-    if cart_id is None:
-        cart_obj = cart_create()
-        request.session['cart_id'] = cart_obj.id
+    qs = Cart.objects.filter(id=cart_id)
+    if qs.count() == 1:
+        print('Cart ID exists')
+        cart_obj = qs.first()
+        if request.user.is_authenticated() and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
-        qs = Cart.objects.filter(id=cart_id)
-        if qs.count() == 1:
-            print('Cart ID exists')
-            cart_obj = qs.first()
-        else:
-            cart_obj = cart_create()
-            request.session['cart_id'] = cart_obj.id
+        cart_obj = Cart.objects.new(user=request.user)
+        request.session['cart_id'] = cart_obj.id
     return render(request, "carts/home.html", {})
