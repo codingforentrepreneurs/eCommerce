20368456287ce253d2b51c7f4d2101f52ceb4d35
CFE <hello@teamcfe.com>
2017-09-25 18:06:52 -0700

    5 - Cart Model Manager Part 2

--- src/carts/models.py
+++ src/carts/models.py
@@ -8,6 +8,21 @@ from products.models import Product
 User = settings.AUTH_USER_MODEL
 
 class CartManager(models.Manager):
+    def new_or_get(self, request):
+        cart_id = request.session.get("cart_id", None)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated() and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Cart.objects.new(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
+
     def new(self, user=None):
         user_obj = None
         if user is not None:
--- src/carts/views.py
+++ src/carts/views.py
@@ -3,15 +3,5 @@ from django.shortcuts import render
 from .models import Cart
 
 def cart_home(request):
-    cart_id = request.session.get("cart_id", None)
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        print('Cart ID exists')
-        cart_obj = qs.first()
-        if request.user.is_authenticated() and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)
-        request.session['cart_id'] = cart_obj.id
+    cart_obj = Cart.objects.new_or_get(request)
     return render(request, "carts/home.html", {})
