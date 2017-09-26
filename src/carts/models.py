c64ba909c1b6af3b5b0502412eef20857cdaa25a
CFE <hello@teamcfe.com>
2017-09-25 18:27:18 -0700

    7 - M2M Changed Signal to Calculate Cart Total

--- src/carts/models.py
+++ src/carts/models.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.db import models
-
+from django.db.models.signals import pre_save, post_save, m2m_changed
 
 from products.models import Product
 
@@ -33,6 +33,7 @@ class CartManager(models.Manager):
 class Cart(models.Model):
     user        = models.ForeignKey(User, null=True, blank=True)
     products    = models.ManyToManyField(Product, blank=True)
+    subtotal    = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     total       = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated     = models.DateTimeField(auto_now=True)
     timestamp   = models.DateTimeField(auto_now_add=True)
@@ -41,3 +42,39 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        products = instance.products.all()
+        total = 0
+        for x in products:
+            total += x.price
+        if instance.subtotal != total:
+            instance.subtotal = total
+            instance.save()
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
+
+
+
+
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+    instance.total = instance.subtotal  + 10 #* 1.08
+
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/carts/views.py
+++ src/carts/views.py
@@ -3,5 +3,12 @@ from django.shortcuts import render
 from .models import Cart
 
 def cart_home(request):
-    cart_obj = Cart.objects.new_or_get(request)
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for x in products:
+        total += x.price
+    print(total)
+    cart_obj.total = total
+    cart_obj.save()
     return render(request, "carts/home.html", {})
