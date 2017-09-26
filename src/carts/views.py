35e8bf075b51d32a7c0fcc1c191700736d64edfc
CFE <hello@teamcfe.com>
2017-09-25 17:49:37 -0700

    4 - Create A Cart in the View

--- src/carts/views.py
+++ src/carts/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 
+from .models import Cart
 
+
+def cart_create(user=None):
+    cart_obj = Cart.objects.create(user=None)
+    print('New Cart created')
+    return cart_obj
 
 def cart_home(request):
+    request.session['cart_id']  = "12"
     cart_id = request.session.get("cart_id", None)
-    if cart_id is None: # and isinstance(cart_id, int):
-        print('create new cart')
-        request.session['cart_id'] = 12
+    if cart_id is None:
+        cart_obj = cart_create()
+        request.session['cart_id'] = cart_obj.id
     else:
-        print('Cart ID exists')
+        qs = Cart.objects.filter(id=cart_id)
+        if qs.count() == 1:
+            print('Cart ID exists')
+            cart_obj = qs.first()
+        else:
+            cart_obj = cart_create()
+            request.session['cart_id'] = cart_obj.id
     return render(request, "carts/home.html", {})
