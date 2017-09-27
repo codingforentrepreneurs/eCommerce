d5cced3c944f39164a36b4ef122ba2369006fc92
CFE <hello@teamcfe.com>
2017-09-26 17:55:50 -0700

    9 - Add to Cart Form

--- src/carts/views.py
+++ src/carts/views.py
@@ -16,13 +16,18 @@ def cart_home(request):
 
 
 def cart_update(request):
-    product_id = 1
-    product_obj = Product.objects.get(id=product_id)
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    if product_obj in cart_obj.products.all():
-        cart_obj.products.remove(product_obj)
-    else:
-        cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
-    
-    # return redirect(product_obj.get_absolute_url())
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            print("Show message to user, product is gone?")
+            return redirect("cart:home")
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
+        
+        # return redirect(product_obj.get_absolute_url())
     return redirect("cart:home")
