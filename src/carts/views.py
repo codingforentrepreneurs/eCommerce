691d5ae69e2b5a8f569be0ede3efd5c9fc7897d3
CFE <hello@teamcfe.com>
2017-09-26 18:30:39 -0700

    12 - Cart Icon & FontAwesome

--- src/carts/views.py
+++ src/carts/views.py
@@ -21,6 +21,6 @@ def cart_update(request):
             cart_obj.products.remove(product_obj)
         else:
             cart_obj.products.add(product_obj) # cart_obj.products.add(product_id)
-        
+        request.session['cart_items'] = cart_obj.products.count()
         # return redirect(product_obj.get_absolute_url())
     return redirect("cart:home")
