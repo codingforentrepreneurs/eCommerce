f546b36eba57d99713408dedd9e76dcb496e8ada
CFE <hello@teamcfe.com>
2017-09-28 13:37:28 -0700

    7 - Upgrading Auth to Prep for Checkout

--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -40,49 +40,6 @@ def contact_page(request):
     return render(request, "contact/view.html", context)
 
 
-def login_page(request):
-    form = LoginForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    print("User logged in")
-    #print(request.user.is_authenticated())
-    if form.is_valid():
-        print(form.cleaned_data)
-        username  = form.cleaned_data.get("username")
-        password  = form.cleaned_data.get("password")
-        user = authenticate(request, username=username, password=password)
-        print(user)
-        #print(request.user.is_authenticated())
-        if user is not None:
-            #print(request.user.is_authenticated())
-            login(request, user)
-            # Redirect to a success page.
-            #context['form'] = LoginForm()
-            return redirect("/")
-        else:
-            # Return an 'invalid login' error message.
-            print("Error")
-
-    return render(request, "auth/login.html", context)
-
-
-User = get_user_model()
-def register_page(request):
-    form = RegisterForm(request.POST or None)
-    context = {
-        "form": form
-    }
-    if form.is_valid():
-        print(form.cleaned_data)
-        username  = form.cleaned_data.get("username")
-        email  = form.cleaned_data.get("email")
-        password  = form.cleaned_data.get("password")
-        new_user  = User.objects.create_user(username, email, password)
-        print(new_user)
-
-    return render(request, "auth/register.html", context)
-
 
 
 
