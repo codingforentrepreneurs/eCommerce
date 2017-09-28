a0e30cbe85971622c10e8d1bbf930dbaa78695d9
CFE <hello@teamcfe.com>
2017-09-28 15:56:31 -0700

    12 - Order Manager

--- src/carts/views.py
+++ src/carts/views.py
@@ -42,25 +42,21 @@ def checkout_home(request):
     login_form = LoginForm()
     guest_form = GuestForm()
     guest_email_id = request.session.get('guest_email_id')
+    
     if user.is_authenticated():
+        'logged in user checkout; remember payment stuff'
         billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(
                                                         user=user, email=user.email)
     elif guest_email_id is not None:
+        'guest user checkout; auto reloads payment stuff'
         guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
         billing_profile, billing_guest_profile_created = BillingProfile.objects.get_or_create(
                                         email=guest_email_obj.email)
     else:
         pass
-    
+
     if billing_profile is not None:
-        order_qs = Order.objects.filter(billing_profile=billing_profile, cart=cart_obj, active=True)
-        if order_qs.count() == 1:
-            order_obj = order_qs.first()
-        else:
-            old_order_qs = Order.objects.exclude(billing_profile=billing_profile).filter(cart=cart_obj, active=True)
-            if old_order_qs.exists():
-                old_order_qs.update(active=False)
-            order_obj = Order.objects.create(billing_profile=billing_profile, cart=cart_obj)
+        order_obj, order_obj_created = Order.objects.new_or_get(billing_profile, cart_obj)
 
     context = {
         "object": order_obj,
--- src/orders/models.py
+++ src/orders/models.py
@@ -13,6 +13,24 @@ ORDER_STATUS_CHOICES = (
     ('refunded', 'Refunded'),
 )
 
+class OrderManager(models.Manager):
+    def new_or_get(self, billing_profile, cart_obj):
+        created = False
+        qs = self.get_queryset().filter(
+                billing_profile=billing_profile, 
+                cart=cart_obj, 
+                active=True)
+        if qs.count() == 1:
+            obj = qs.first()
+        else:
+            obj = self.model.objects.create(
+                    billing_profile=billing_profile, 
+                    cart=cart_obj)
+            created = True
+        return obj, created
+
+
+
 # Random, Unique
 class Order(models.Model):
     billing_profile = models.ForeignKey(BillingProfile, null=True, blank=True)
@@ -29,6 +47,8 @@ class Order(models.Model):
     def __str__(self):
         return self.order_id
 
+    objects = OrderManager()
+
     def update_total(self):
         cart_total = self.cart.total
         shipping_total = self.shipping_total
@@ -42,6 +62,10 @@ class Order(models.Model):
 def pre_save_create_order_id(sender, instance, *args, **kwargs):
     if not instance.order_id:
         instance.order_id = unique_order_id_generator(instance)
+    qs = Order.objects.filter(cart=instance.cart).exclude(billing_profile=instance.billing_profile)
+    if qs.exists():
+        qs.update(active=False)
+
 
 pre_save.connect(pre_save_create_order_id, sender=Order)
 
