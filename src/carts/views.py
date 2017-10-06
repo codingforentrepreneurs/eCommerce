9455685b79a7ba829799d6dc700dce71c930c5d3
CFE <hello@teamcfe.com>
2017-10-06 15:53:29 -0700

    4 - Cart Item Count

--- src/carts/views.py
+++ src/carts/views.py
@@ -41,6 +41,7 @@ def cart_update(request):
             json_data = {
                 "added": added,
                 "removed": not added,
+                "cartItemCount": cart_obj.products.count()
             }
             return JsonResponse(json_data)
     return redirect("cart:home")
