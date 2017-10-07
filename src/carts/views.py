c16e30dbd9af01fddef5614f3857de4a7fcace3b
CFE <hello@teamcfe.com>
2017-10-06 17:44:05 -0700

    8 - Finalize Cart Updating with Ajax

--- src/carts/views.py
+++ src/carts/views.py
@@ -16,12 +16,13 @@ from .models import Cart
 
 def cart_detail_api_view(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    products = [{"name": x.name, "price": x.price} for x in cart_obj.products.all()] # [<object>, <object>, <object>]
-    # products_list = []
-    # for x in cart_obj.products.all():
-    #     products_list.append(
-    #             {"name": x.name, "price": x.price}
-    #         )
+    products = [{
+            "id": x.id,
+            "url": x.get_absolute_url(),
+            "name": x.name, 
+            "price": x.price
+            } 
+            for x in cart_obj.products.all().reverse()]
     cart_data  = {"products": products, "subtotal": cart_obj.subtotal, "total": cart_obj.total}
     return JsonResponse(cart_data)
 
--- src/products/views.py
+++ src/products/views.py
@@ -34,6 +34,12 @@ class ProductListView(ListView):
     #     print(context)
     #     return context
 
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductListView, self).get_context_data(*args, **kwargs)
+        cart_obj, new_obj = Cart.objects.new_or_get(self.request)
+        context['cart'] = cart_obj
+        return context
+
     def get_queryset(self, *args, **kwargs):
         request = self.request
         return Product.objects.all()
