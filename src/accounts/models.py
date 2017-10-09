968ec0525e27c3d95f9fc033833264437f90a47b
CFE <hello@teamcfe.com>
2017-10-09 14:04:01 -0700

    3 - Create the User Model Manager

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,7 +1,43 @@
 from django.db import models
 from django.contrib.auth.models import (
-    AbstractBaseUser
+    AbstractBaseUser, BaseUserManager
 )
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, is_active=True, is_staff=False, is_admin=False):
+        if not email:
+            raise ValueError("Users must have an email address")
+        if not password:
+            raise ValueError("Users must have a password")
+
+        user_obj = self.model(
+            email = self.normalize_email(email)
+        )
+        user_obj.set_password(password) # change user password
+        user_obj.staff = is_staff
+        user_obj.admin = is_admin
+        user_obj.active = is_active
+        user_obj.save(using=self._db)
+        return user_obj
+
+    def create_staffuser(self, email, password=None):
+        user = self.create_user(
+                email,
+                password=password,
+                is_staff=True
+        )
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+                email,
+                password=password,
+                is_staff=True,
+                is_admin=True
+        )
+        return user
+
 
 class User(AbstractBaseUser):
     email       = models.EmailField(max_length=255, unique=True)
@@ -16,6 +52,8 @@ class User(AbstractBaseUser):
     USERNAME_FIELD = 'email' #username
     # USERNAME_FIELD and password are required by default
     REQUIRED_FIELDS = [] #['full_name'] #python manage.py createsuperuser
+
+    objects = UserManager()
 
     def __str__(self):
         return self.email
