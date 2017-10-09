c698e75c82b6145546c93f8d32fec616dfaf1cac
CFE <hello@teamcfe.com>
2017-10-09 13:57:09 -0700

    2 - Create the Abstract Base User

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,4 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser
+)
+
+class User(AbstractBaseUser):
+    email       = models.EmailField(max_length=255, unique=True)
+    # full_name   = models.CharField(max_length=255, blank=True, null=True)
+    active      = models.BooleanField(default=True) # can login 
+    staff       = models.BooleanField(default=False) # staff user non superuser
+    admin       = models.BooleanField(default=False) # superuser 
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    # confirm     = models.BooleanField(default=False)
+    # confirmed_date     = models.DateTimeField(default=False)
+
+    USERNAME_FIELD = 'email' #username
+    # USERNAME_FIELD and password are required by default
+    REQUIRED_FIELDS = [] #['full_name'] #python manage.py createsuperuser
+
+    def __str__(self):
+        return self.email
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+
+    @property
+    def is_active(self):
+        return self.active
+
+
 
 
 
