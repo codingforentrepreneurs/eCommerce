ca4245aabaacc0f3c0f7e6e77902f0312a30686e
CFE <hello@teamcfe.com>
2017-10-11 14:07:25 -0700

    4 - Improving Payment Method Form

--- src/billing/views.py
+++ src/billing/views.py
@@ -1,15 +1,26 @@
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
-
+from django.utils.http import is_safe_url
 
 
 import stripe
 stripe.api_key = "sk_test_cu1lQmcg1OLffhLvYrSCp5XE"
-
-
 STRIPE_PUB_KEY = 'pk_test_PrV61avxnHaWIYZEeiYTTVMZ'
 
 
 def payment_method_view(request):
-    if request.method == "POST":
+    #next_url = 
+    next_url = None
+    next_ = request.GET.get('next')
+    if is_safe_url(next_, request.get_host()):
+        next_url = next_
+    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY, "next_url": next_url})
+
+
+
+
+def payment_method_createview(request):
+    if request.method == "POST" and request.is_ajax():
         print(request.POST)
-    return render(request, 'billing/payment-method.html', {"publish_key": STRIPE_PUB_KEY})
+        return JsonResponse({"message": "Done"})
+    return HttpResponse("error", status_code=401)
--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -25,7 +25,7 @@ from django.views.generic import TemplateView
 
 from accounts.views import LoginView, RegisterView, guest_register_view
 from addresses.views import checkout_address_create_view, checkout_address_reuse_view
-from billing.views import payment_method_view
+from billing.views import payment_method_view, payment_method_createview
 from carts.views import cart_detail_api_view
 
 from .views import home_page, about_page, contact_page
@@ -42,6 +42,7 @@ urlpatterns = [
     url(r'^api/cart/$', cart_detail_api_view, name='api-cart'),
     url(r'^cart/', include("carts.urls", namespace='cart')),
     url(r'^billing/payment-method/$', payment_method_view, name='billing-payment-method'),
+    url(r'^billing/payment-method/create/$', payment_method_createview, name='billing-payment-method-endpoint'),
     url(r'^register/$', RegisterView.as_view(), name='register'),
     url(r'^bootstrap/$', TemplateView.as_view(template_name='bootstrap/example.html')),
     url(r'^products/', include("products.urls", namespace='products')),
