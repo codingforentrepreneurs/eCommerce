8decd9aa8de59957dcddd8c469845f7827e06c95
CFE <hello@teamcfe.com>
2017-10-19 13:43:17 -0700

    Added AWS Confgi

---
+++ src/ecommerce/aws/conf.py
@@ -0,0 +1,30 @@
+import datetime
+
+AWS_GROUP_NAME = "CFE_eCommerce_Group"
+AWS_USERNAME = "cfe-ecommerce-user"
+AWS_ACCESS_KEY_ID = "AKIAJARK375PALZJC55Q"
+AWS_SECRET_ACCESS_KEY = "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"
+
+AWS_FILE_EXPIRE = 200
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = False
+
+DEFAULT_FILE_STORAGE = 'ecommerce.aws.utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'ecommerce.aws.utils.StaticRootS3BotoStorage'
+AWS_STORAGE_BUCKET_NAME = 'cfe-ecommerce'
+S3DIRECT_REGION = 'us-west-2'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + 'static/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+
+two_months = datetime.timedelta(days=61)
+date_two_months_later = datetime.date.today() + two_months
+expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+
+AWS_HEADERS = { 
+ 'Expires': expires,
+ 'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+}
+
---
+++ src/ecommerce/aws/utils.py
@@ -0,0 +1,4 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+StaticRootS3BotoStorage = lambda: S3Boto3Storage(location='static')
+MediaRootS3BotoStorage  = lambda: S3Boto3Storage(location='media')
