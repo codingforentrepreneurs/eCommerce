f776fc8227c5717b7982274d5de2a12676ab835b
CFE <hello@teamcfe.com>
2017-10-19 10:02:13 -0700

    2 - New Settings Module

--- src/ecommerce/settings/base.py
+++ src/ecommerce/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -59,7 +59,7 @@ FORCE_INACTIVE_USER_ENDSESSION= False
 
 
 
-MAILCHIMP_API_KEY = "1fbc8637b28ac0caa5f5ad9fe4337f69-us17"
+MAILCHIMP_API_KEY = "717d0854ed20fed3be3689a3f125915c-us17"
 MAILCHIMP_DATA_CENTER = "us17"
 MAILCHIMP_EMAIL_LIST_ID = "e2ef12efee"
 
