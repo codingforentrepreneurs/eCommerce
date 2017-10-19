a2041155d977f6d0451aa6220fec0f664bc0cb06
CFE <hello@teamcfe.com>
2017-10-19 11:11:13 -0700

    8 - Deploy to Heroku

--- src/ecommerce/settings/production.py
+++ src/ecommerce/settings/production.py
@@ -110,6 +110,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config() #postgreSQL Database in heroku
+DATABASES['default'].update(db_from_env)
+DATABASES['default']['CONN_MAX_AGE'] = 500
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
