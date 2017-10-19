f65d67029ebad9a22f6e63ef66170e533a385f2e
CFE <hello@teamcfe.com>
2017-10-19 10:49:47 -0700

    7 - Setup Git Version Control

--- src/ecommerce/settings/production.py
+++ src/ecommerce/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@!-)jwmuzh8btr380g61=g+#&zzei&dz2(&=xbvxztady)_p(r'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
